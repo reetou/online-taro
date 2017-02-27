d2aff23b997c11b3f92390b2fad147457e0d29e8
root <root@shkola-sync.ru>
2017-02-27 12:02:54 +0300

    More detailed repo

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import Deck from './App';
+import './index.css';
+
+ReactDOM.render(
+  <Deck />,
+  document.getElementById('root')
+);
